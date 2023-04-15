f52be3a300c7d6f3c7ed9f15c2e929dea5683d2e
David Olagookun <erijesudo@gmail.com>
2023-04-15 18:24:56 +0100

    Initial Commit

---
+++ TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by David OH on 15/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
